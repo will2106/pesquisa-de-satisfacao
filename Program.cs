9a1aacc8d1d8ccf90152778d54bbae50039e9fa0
william santos <willdev0904@gmail.com>
2023-04-10 07:58:35 -0300

    atividade pesquisa de satisfacao

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿//programa de pesquisa de satisfacao incluindo como informacao sexo do entrevistado e opniao sobre o produto.
+
+
+Console.WriteLine($"Ola seja bem vindo!");
+Console.WriteLine($"Por favor, responda nossa pesquisa de satisfacao: ");
+
+//entradas sexo e opiniao se gostou ou nao do produto.
+
+
+
+
+
+//entradas quantidade de homem e mulheres que gostaram e nao gostarm
+
+
+//processamento 
+
+for (int i = 0; i <=10; i++)
+{
+    Console.WriteLine($"Qual o seu sexo? digite 'm' para Mulher e 'h' para Homem ");
+    char sexo= char.Parse(Console.ReadLine());
+    Console.WriteLine($"Voce gostou do nosso produto? digite 's' para Sim e 'n' para Nao: ");
+    char gostou= char.Parse(Console.ReadLine());
+
+}
